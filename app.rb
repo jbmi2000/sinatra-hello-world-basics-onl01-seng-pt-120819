1ad4be23e2f43bdfd99f86e5994823bd77339154
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-16 01:34:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
+  get '/' do
+    "Hello, World!"
+  end
   # Define your GET '/' route below and respond with "Hello, World!"
-
-
 end
