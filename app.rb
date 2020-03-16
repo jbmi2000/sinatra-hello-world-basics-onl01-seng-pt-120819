632a762c56e9df50c018e68ac4dc3bb0a6cf42e5
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-16 01:32:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "Hello, World"
+  end
   # Define your GET '/' route below and respond with "Hello, World!"
-
 end
